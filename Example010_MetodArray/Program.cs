1d8d2c82f120c50ba9a71aeb4843c825af834427
Maria Yutaeva <mariya1982@yandex.ru>
2022-09-19 14:36:39 +0300

    Задача с массивом

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {11,24,28,8,57,60,67,8};
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+while(index <n)
+{
+    if(array[index] == find)
+    {
+    Console.WriteLine(index);
+    break; // прервать проверку
+    }
+    index++;
+}
